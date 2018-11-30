36abf8a86915d5a44aa22cd74379e8a9da4b3279
Vladyslav Omelyanenko <vladyslavomelyanenko@MacBook-Pro-Vladyslav.local>
2018-11-30 18:20:38 +0200

    figured out w / operator precedence and created a test

--- Homework6/index.test.js
+++ Homework6/index.test.js
@@ -11,3 +11,9 @@ expect(Number.isInteger((9.655).toFixed(2))).toEqual(false);
 test('test_arrays', () => {
 expect(Number(parseFloat([1, 23, -5, 47, 2, 8.5, -12].sort().reverse().join('')).toFixed(0))).toEqual(9);
 });
+
+var n = 3;
+
+test('test_compare_operators', () => {
+    expect(n++ * 8 - n > 28 ).toEqual(true);
+    });
