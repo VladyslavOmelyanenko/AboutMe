36f539256f895a7e51b11aeaadab516c49e3f5b0
Vladyslav Omelyanenko <vladyslavomelyanenko@MacBook-Pro-Vladyslav.local>
2018-11-30 18:07:47 +0200

    added tests for methods and properties String, Number, Array

---
+++ Homework6/index.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+test('test_string', () => {
+expect(("My name is Vlad").split(' ').reverse().join(' ')).toEqual("Vlad is name My");
+});
+
+test('test_numbers', () => {
+expect(Number.isInteger((9.655).toFixed(2))).toEqual(false);
+});
+
+test('test_arrays', () => {
+expect(Number(parseFloat([1, 23, -5, 47, 2, 8.5, -12].sort().reverse().join('')).toFixed(0))).toEqual(9);
+});
